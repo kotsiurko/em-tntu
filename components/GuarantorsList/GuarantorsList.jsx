a51f92fffa57d69f8a4f1fa6c34eca32395cae0a
Roman <kotsiurko@gmail.com>
2023-12-15 15:41:56 +0200

    Update GuarantorsList.jsx

--- components/GuarantorsList/GuarantorsList.jsx
+++ components/GuarantorsList/GuarantorsList.jsx
@@ -1,25 +1,11 @@
-// import { urlFor } from "../../../lib/client";
-// import { urlFor } from "@/lib/client";
-// import Image from "next/image";
-// import Link from "next/link";
-// import styles from "./GuarantatorList.module.css";
-// import { personCredentials } from "@/lib/helpers";
-// import DocsViewer from "../DocsViewer/DocsViewer";
-// import { useState } from "react";
 import GuarantorsItem from "./GuarantorsItem";
 
 function GuarantorsList({ personList }) {
-  console.log("personList in GuarantorsList:>> ", personList);
-
-  // const [isOPPOpen, setIsOPPOpen] = useState(false);
-  // const [openedOPP, setOpenedOPP] = useState();
-
   return (
     <section className="features guaranors">
       <div className="container aos-init aos-animate" data-aos="fade-up">
         {personList.map((el) => {
           const { firstName, secondName } = el;
-          console.log("el :>> ", el);
 
           return (
             <GuarantorsItem person={el} key={`${firstName} ${secondName}`} />
