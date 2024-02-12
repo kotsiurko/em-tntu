d5b33dff44d86c2c40a14ac1789a0a35bd760d99
Roman <kotsiurko@gmail.com>
2024-02-12 18:18:16 +0200

    clear commented code

--- pages/science/conference-lighting-and-power-engineering/[slug].js
+++ pages/science/conference-lighting-and-power-engineering/[slug].js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
-// import { useRouter } from "next/router";
 
 // Client connection
 import { client } from "lib/client";
@@ -9,48 +8,22 @@ import { client } from "lib/client";
 import { menuItems } from "components/Header/menuItems";
 import {
   mainMenuQueriesObjCreator,
-  // chapterPageQuery,
   slugCurrent,
   chapterItemQuery,
 } from "lib/queries";
 import { menuCreator, menuItemsMerger } from "lib/menuCreator";
-// import { getPortion } from "lib/helpers";
 
 // Components
 import Header from "components/Header/Header";
 import { Breadcrumbs } from "components/Breadcrumbs/Breadcrumbs";
 import PageContentSection from "components/PageContentSection/PageContentSection";
-import { } from "../../../lib/queries";
+import { } from "lib/queries";
 
 const LEConference = ({
   chapterConferencePage,
-  // totalNewsAmount,
-  // initArr,
   mainMenuQO,
 }) => {
-  console.log('chapterConferencePage :>> ', chapterConferencePage);
   const { title, slug, metaDescription } = chapterConferencePage;
-  // const router = useRouter();
-
-  // const [resultQuery, setResultQuery] = useState();
-  // const [currPage, setCurrPage] = useState();
-
-  // useEffect(() => {
-  //   if (router.asPath.includes("?page=")) {
-  //     // розрізаю стрічку адреси пополам і дістаю з неї праву частину
-  //     const pageNum = parseInt(router.asPath.split("?page=")[1]);
-  //     setCurrPage(pageNum);
-  //     getData(pageNum);
-  //   } else {
-  //     setCurrPage(1);
-  //     getData(1);
-  //   }
-  // }, [router.asPath]);
-
-  // async function getData(page) {
-  //   const res = await getPortion(page, newsBool);
-  //   setResultQuery(res);
-  // }
 
   // MENU FORMATION PART ==============================================
 
@@ -88,30 +61,6 @@ const LEConference = ({
       />
 
       <PageContentSection data={chapterConferencePage} />
-
-      {/* ======= Inner Page Team-Staff Section ======= */}
-      {/* <section id="team" className="team">
-        <div className="container" data-aos="fade-up">
-          <header className="section-header">
-            <p>НОВИНИ РОЗДІЛУ</p>
-          </header>
-
-          <div className="row gy-4">
-            <NewsItems currentItems={resultQuery} />
-          </div>
-
-          {totalNewsAmount > newsPerPage && (
-            <NewPagination
-              totalNewsAmount={totalNewsAmount}
-              currPage={currPage}
-              setResultQuery={setResultQuery}
-              setCurrPage={setCurrPage}
-              newsBool={newsBool}
-            />
-          )}
-        </div>
-      </section> */}
-      {/* ======= End Team-Staff Page Section ======= */}
     </>
   );
 };
@@ -133,13 +82,9 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  // console.log('slug :>> ', slug);
   const chapterConferencePage = await client.fetch(
     chapterItemQuery("scienceLEConference", `/science/conference-lighting-and-power-engineering/${slug}`)
   );
-  // const chapterConferencePage = await client.fetch(
-  //   chapterPageQuery("scienceLEConference", slug)
-  // );
   const mainMenuQO = await mainMenuQueriesObjCreator();
 
   return {
