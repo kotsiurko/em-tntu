8be8500984250a9cc3c76e7fce7b4c7187b256d8
Roman Kotsiurko <kotsiurko@gmail.com>
2024-04-06 01:27:27 +0300

    Update [slug].js

--- pages/international-activity/[slug].js
+++ pages/international-activity/[slug].js
@@ -1,22 +1,27 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
+import { useEffect, useState } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 // Client connection
 import { client } from "lib/client";
 
 // Helpers
-import { menuItems } from 'components/Header/menuItems';
-import { mainMenuQueriesObjCreator, chapterPageQuery, slugCurrent, newsPerPage } from 'lib/queries';
-import { menuCreator, menuItemsMerger } from 'lib/menuCreator';
+import { menuItems } from "components/Header/menuItems";
+import {
+  mainMenuQueriesObjCreator,
+  chapterPageQuery,
+  slugCurrent,
+  newsPerPage,
+} from "lib/queries";
+import { menuCreator, menuItemsMerger } from "lib/menuCreator";
 import { getPortion } from "lib/helpers";
 
 // Components
-import Header from 'components/Header/Header';
+import Header from "components/Header/Header";
 import { Breadcrumbs } from "components/Breadcrumbs/Breadcrumbs";
-import PageContentSection from 'components/PageContentSection/PageContentSection';
-import NewsItems from 'components/NewsItems/NewsItems';
-import ImageTextItems from 'components/ImageTextItems/ImageTextItems';
+import PageContentSection from "components/PageContentSection/PageContentSection";
+import NewsItems from "components/NewsItems/NewsItems";
+import ImageTextItems from "components/ImageTextItems/ImageTextItems";
 import NewPagination from "components/Pagination/NewPagination";
 
 // -----------------------------------------------------------------
@@ -30,13 +35,8 @@ const InternationalActivityPage = ({
   totalNewsAmountECF,
   totalNewsAmountPTP,
 }) => {
-
-  const {
-    title,
-    slug,
-    metaDescription,
-    intProgsTrainsAndProjects,
-  } = internationalActivityPage;
+  const { title, slug, metaDescription, intProgsTrainsAndProjects } =
+    internationalActivityPage;
   const router = useRouter();
 
   const [resultQuery, setResultQuery] = useState();
@@ -48,27 +48,34 @@ const InternationalActivityPage = ({
     async function getData(page) {
       let res;
       if (slug.current === "/international-activity/academic-mobility") {
-        setNewsBool("academicMobilityBool")
+        setNewsBool("academicMobilityBool");
         setTotalNewsAmount(totalNewsAmountAcadMobil);
         res = await getPortion(page, "academicMobilityBool");
       }
-      if (slug.current === "/international-activity/international-practice-of-students") {
-        setNewsBool("intPractOfStudentsBool")
+      if (
+        slug.current ===
+        "/international-activity/international-practice-of-students"
+      ) {
+        setNewsBool("intPractOfStudentsBool");
         setTotalNewsAmount(totalNewsAmountIntPractOfStudents);
         res = await getPortion(page, "intPractOfStudentsBool");
       }
       if (slug.current === "/international-activity/international-internship") {
-        setNewsBool("intInternshipBool")
+        setNewsBool("intInternshipBool");
         setTotalNewsAmount(totalNewsAmountIntInternship);
         res = await getPortion(page, "intInternshipBool");
       }
-      if (slug.current === "/international-activity/events-conferences-forums") {
-        setNewsBool("eventsConferencesForumsBool")
+      if (
+        slug.current === "/international-activity/events-conferences-forums"
+      ) {
+        setNewsBool("eventsConferencesForumsBool");
         setTotalNewsAmount(totalNewsAmountECF);
         res = await getPortion(page, "eventsConferencesForumsBool");
       }
-      if (slug.current === "/international-activity/programs-trainings-projects") {
-        setNewsBool("programsTrainingsProjectsBool")
+      if (
+        slug.current === "/international-activity/programs-trainings-projects"
+      ) {
+        setNewsBool("programsTrainingsProjectsBool");
         setTotalNewsAmount(totalNewsAmountPTP);
         res = await getPortion(page, "programsTrainingsProjectsBool");
       }
@@ -84,10 +91,15 @@ const InternationalActivityPage = ({
       setCurrPage(1);
       getData(1);
     }
-  }, [router.asPath, slug, totalNewsAmountAcadMobil, totalNewsAmountECF, totalNewsAmountIntInternship, totalNewsAmountIntPractOfStudents, totalNewsAmountPTP]);
-
-
-
+  }, [
+    router.asPath,
+    slug,
+    totalNewsAmountAcadMobil,
+    totalNewsAmountECF,
+    totalNewsAmountIntInternship,
+    totalNewsAmountIntPractOfStudents,
+    totalNewsAmountPTP,
+  ]);
 
   // MENU FORMATION PART ==============================================
 
@@ -127,89 +139,74 @@ const InternationalActivityPage = ({
       </section>
 
       {/* Page Content */}
-      {slug.current !== "/international-activity/international-partners" &&
+      {slug.current !==
+        "/international-activity/programs-trainings-projects" && (
         <PageContentSection data={internationalActivityPage} />
-      }
+      )}
 
-      {slug.current === "/international-activity/programs-trainings-projects" && <section id="team" className="team">
-        <div className="container" data-aos="fade-up">
-          {/* <header className="section-header">
-            <p>Події розділу</p>
-          </header> */}
-
-
-          <div className="row gy-4">
-            <ImageTextItems currentItems={intProgsTrainsAndProjects} />
+      {slug.current ===
+        "/international-activity/programs-trainings-projects" && (
+        <section id="team" className="team">
+          <div className="container" data-aos="fade-up">
+            <div className="row gy-4">
+              <ImageTextItems currentItems={intProgsTrainsAndProjects} />
+            </div>
           </div>
-
-          {/* PAGINATION BLOCK STARTS */}
-          {/* {(totalNewsAmount > newsPerPage) && (
-            <NewPagination
-              totalNewsAmount={totalNewsAmount}
-              currPage={currPage}
-              setResultQuery={setResultQuery}
-              setCurrPage={setCurrPage}
-              newsBool={newsBool}
-            />
-          )} */}
-          {/* PAGINATION BLOCK ENDS */}
-
-
-        </div>
-      </section>}
+        </section>
+      )}
 
       {/* ======= Inner Page Team-Staff Section ======= */}
-      {slug.current !== "/international-activity/international-partners" && <section id="team" className="team">
-        <div className="container" data-aos="fade-up">
-          <header className="section-header">
-            <p>Події розділу</p>
-          </header>
+      {slug.current !== "/international-activity/international-partners" && (
+        <section id="team" className="team">
+          <div className="container" data-aos="fade-up">
+            <header className="section-header">
+              <p>Події розділу</p>
+            </header>
 
+            <div className="row gy-4">
+              <NewsItems currentItems={resultQuery} />
+            </div>
 
-          <div className="row gy-4">
-            <NewsItems currentItems={resultQuery} />
+            {/* PAGINATION BLOCK STARTS */}
+            {totalNewsAmount > newsPerPage && (
+              <NewPagination
+                totalNewsAmount={totalNewsAmount}
+                currPage={currPage}
+                setResultQuery={setResultQuery}
+                setCurrPage={setCurrPage}
+                newsBool={newsBool}
+              />
+            )}
+            {/* PAGINATION BLOCK ENDS */}
           </div>
-
-          {/* PAGINATION BLOCK STARTS */}
-          {(totalNewsAmount > newsPerPage) && (
-            <NewPagination
-              totalNewsAmount={totalNewsAmount}
-              currPage={currPage}
-              setResultQuery={setResultQuery}
-              setCurrPage={setCurrPage}
-              newsBool={newsBool}
-            />
-          )}
-          {/* PAGINATION BLOCK ENDS */}
-
-
-        </div>
-      </section>}
+        </section>
+      )}
       {/* ======= End Team-Staff Page Section ======= */}
-
     </>
-  )
-}
-
+  );
+};
 
 export default InternationalActivityPage;
 
 export async function getStaticPaths() {
-  const query = slugCurrent('international-activity');
+  const query = slugCurrent("international-activity");
 
   const pages = await client.fetch(query);
   const paths = pages.map((page) => ({
     params: {
-      slug: page.slug.current
-    }
+      slug: page.slug.current,
+    },
   }));
   return {
-    paths, fallback: 'blocking'
-  }
+    paths,
+    fallback: "blocking",
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const internationalActivityPage = await client.fetch(chapterPageQuery('international-activity', slug));
+  const internationalActivityPage = await client.fetch(
+    chapterPageQuery("international-activity", slug)
+  );
 
   const totalNewsAmountAcadMobil = await client.fetch(
     `count(*[_type == "news" && academicMobilityBool])`
