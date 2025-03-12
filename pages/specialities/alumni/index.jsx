43285bbd889db191a46d09599230b66657556919
Roman Kotsiurko <kotsiurko@gmail.com>
2025-03-12 22:11:59 +0200

    add external alumni page

---
+++ pages/specialities/alumni/index.jsx
@@ -0,0 +1,129 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+// Client connection
+import { client } from "lib/client";
+
+// Helpers
+import { menuItems } from "components/Header/menuItems";
+import { mainMenuQueriesObjCreator, newsPerPage } from "lib/queries";
+import { menuCreator, menuItemsMerger } from "lib/menuCreator";
+import { getPortion } from "lib/helpers";
+
+// Components
+import Header from "components/Header/Header";
+import { Breadcrumbs } from "components/Breadcrumbs/Breadcrumbs";
+import NewsItems from "components/NewsItems/NewsItems";
+import NewPagination from "components/Pagination/NewPagination";
+
+const newsBool = "alumniBool";
+
+// -----------------------------------------------------------------
+// ------ Page STARTS here -----------------------------------------
+
+const ThematicExcursionNews = ({ totalNewsAmount, mainMenuQO }) => {
+  const router = useRouter();
+
+  const [resultQuery, setResultQuery] = useState();
+  const [currPage, setCurrPage] = useState();
+
+  useEffect(() => {
+    if (router.asPath.includes("?page=")) {
+      // розрізаю стрічку адреси пополам і дістаю з неї праву частину
+      const pageNum = parseInt(router.asPath.split("?page=")[1]);
+      setCurrPage(pageNum);
+      getData(pageNum);
+    } else {
+      setCurrPage(1);
+      getData(1);
+    }
+  }, [router.asPath]);
+
+  async function getData(page) {
+    const res = await getPortion(page, newsBool);
+    setResultQuery(res);
+  }
+
+  // MENU FORMATION PART ==============================================
+
+  const [mainMenuArr, setMainMenuArr] = useState(menuItems);
+
+  useEffect(() => {
+    const menuObj = menuItemsMerger(menuItems, mainMenuQO);
+
+    setMainMenuArr((prevState) => {
+      if (prevState) {
+        return menuCreator(menuObj, prevState);
+      }
+    });
+  }, [mainMenuQO]);
+
+  // MENU FORMATION PART ENDS =========================================
+
+  return (
+    <>
+      <Head>
+        <title>Випускники | Кафедра електричної інженерії ТНТУ</title>
+        <meta
+          name="description"
+          content="Випускники кафедри електричної інженерії"
+        />
+      </Head>
+
+      {/* В хедер треба передавати вже сформований масив */}
+      <Header mainMenuArr={mainMenuArr} />
+
+      {/* <!-- ======= Breadcrumbs ======= --> */}
+      <Breadcrumbs
+        chapterTitle="Спеціальності"
+        pageTitle="Випускники кафедри"
+        pageUrl="alumni"
+      />
+
+      <section className="features my-personal">
+        <div className="row feature-icons">
+          <h3>Випускники кафедри (inner page)</h3>
+        </div>
+      </section>
+
+      {/* ======= Inner Page Team-Staff Section ======= */}
+      <section id="team" className="team">
+        <div className="container" data-aos="fade-up">
+          <div className="row gy-4">
+            <NewsItems currentItems={resultQuery} />
+          </div>
+
+          {/* PAGINATION BLOCK STARTS */}
+          {totalNewsAmount > newsPerPage && (
+            <NewPagination
+              totalNewsAmount={totalNewsAmount}
+              currPage={currPage}
+              setResultQuery={setResultQuery}
+              setCurrPage={setCurrPage}
+              newsBool={newsBool}
+            />
+          )}
+          {/* PAGINATION BLOCK ENDS */}
+        </div>
+      </section>
+      {/* ======= End Team-Staff Page Section ======= */}
+    </>
+  );
+};
+
+export default ThematicExcursionNews;
+
+export async function getStaticProps() {
+  const totalNewsAmount = await client.fetch(
+    `count(*[_type == "news" && ${newsBool}])`
+  );
+  const mainMenuQO = await mainMenuQueriesObjCreator();
+
+  return {
+    props: {
+      totalNewsAmount,
+      mainMenuQO,
+    },
+  };
+}
